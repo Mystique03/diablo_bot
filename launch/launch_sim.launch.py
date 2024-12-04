5cdf5e44c44bdf89d7b35ffbc6df30c27ad4dfba
Smitha <smithasrinivas023@gmail.com>
2024-12-04 12:50:13 +0800

    New Files Added

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,19 +4,16 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration,TextSubstitution,PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable
+from launch_ros.substitutions import FindPackageShare
 
 from launch_ros.actions import Node
 
 
 
 def generate_launch_description():
-
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='diablo_bot'
 
@@ -26,53 +23,66 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    default_world = os.path.join(
-        get_package_share_directory(package_name),
-        'worlds',
-        'empty.world'
+    '''joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    world = LaunchConfiguration('world')
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )'''
 
-    world_arg = DeclareLaunchArgument(
-        'world',
-        default_value=default_world,
-        description='world to load'
-    )
+    #gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
-    # Include the Gazebo launch file, provided by the ros_gz_sim package
+    # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': TextSubstitution(text=f"-r -v4 {default_world}"), 'on_exit_shutdown': 'true'}.items()
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    #launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
-    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-name', 'diablo_robot',
-                                   '-z', '0.1'],
+                                   '-entity', 'my_bot'],
                         output='screen')
     
-    bridge_params = os.path.join(get_package_share_directory(package_name), 'config', 'gz_bridge.yaml')
-    ros_gz_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=[
-            '--ros-args',
-            '-p',
-            f'config_file:={bridge_params}',
-        ]
+
+    joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
     )
 
+    diff_drive_base_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'diff_drive_base_controller'],
+        output='screen'
+    )
 
-
+    joint_state_broadcaster2= Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"]
+    )
+    
+    diff_drive_base_controller2 = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_base_controller"],
+    )
+    
 
     # Launch them all!
     return LaunchDescription([
-        rsp,
         gazebo,
-        world_arg,
+        rsp,
         spawn_entity,
-        ros_gz_bridge
+        joint_state_broadcaster2,
+        diff_drive_base_controller2
     ])
