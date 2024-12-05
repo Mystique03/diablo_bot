7cc9a9ee26b6404868215cb5307d28f4107cf775
Smitha <smithasrinivas023@gmail.com>
2024-12-05 11:00:13 +0800

    Added Controller file

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,10 +4,11 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable
 from launch_ros.substitutions import FindPackageShare
+from launch.event_handlers import OnProcessExit
 
 from launch_ros.actions import Node
 
@@ -42,47 +43,59 @@ def generate_launch_description():
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
                     #launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-entity', 'diffbot'],
                         output='screen')
     
 
-    joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
 
-    diff_drive_base_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'diff_drive_base_controller'],
-        output='screen'
-    )
-
-    joint_state_broadcaster2= Node(
+    joint_state_broadcaster= Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster"]
     )
     
-    diff_drive_base_controller2 = Node(
+    diff_drive_base_controller = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diff_drive_base_controller"],
     )
-    
 
+    trajectory_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["trajectory_controller"]
+    )
+    
     # Launch them all!
     return LaunchDescription([
-        gazebo,
+
         rsp,
+        gazebo,
         spawn_entity,
-        joint_state_broadcaster2,
-        diff_drive_base_controller2
+
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[joint_state_broadcaster],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster,
+                on_exit=[trajectory_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=trajectory_controller,
+                on_exit=[diff_drive_base_controller],
+            )
+        ),
     ])
