fc21681e7080d14236d63a2dba5af08cda053009
Smitha <smithasrinivas023@gmail.com>
2024-12-09 14:27:29 +0800

    Updated config file

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -28,29 +28,36 @@ def generate_launch_description():
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','joystick.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
-    )
+    )'''
 
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     twist_mux = Node(
             package="twist_mux",
             executable="twist_mux",
-            parameters=[twist_mux_params, {'use_sim_time': True}],
+            parameters=[ {'use_sim_time': True}],
             remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )'''
+        )
 
-    #gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-                    #launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'diffbot'],
+                                   '-entity', 'diffbot',
+                                   '-x', '0.0',
+                                   '-y', '0.0',
+                                   '-z', '0.495',
+                                   '-R', '0.0',
+                                   '-P', '0.0',
+                                   '-Y', '0.0',
+                                   ],
                         output='screen')
     
 
@@ -72,6 +79,30 @@ def generate_launch_description():
         executable="spawner",
         arguments=["trajectory_controller"]
     )
+
+    forward_velocity_controller = Node(
+        package = "controller_manager",
+        executable= "spawner",
+        arguments=["forward_velocity_controller"]
+    )
+
+    forward_position_controller = Node(
+        package = "controller_manager",
+        executable= "spawner",
+        arguments=["forward_position_controller"]
+    )
+
+    position_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["position_controller"]
+    )
+
+    velocity_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["velocity_controller"]
+    )
     
     # Launch them all!
     return LaunchDescription([
@@ -79,23 +110,7 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
-
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=spawn_entity,
-                on_exit=[joint_state_broadcaster],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster,
-                on_exit=[trajectory_controller],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=trajectory_controller,
-                on_exit=[diff_drive_base_controller],
-            )
-        ),
+        joint_state_broadcaster,
+        position_controller,
+        velocity_controller,
     ])
